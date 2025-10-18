c35feb6ab7e11149858f3e7ce8b1fae333c7c71c
Vurv <vurvdevelops@gmail.com>
2025-10-17 19:08:43 -0700

    Try a different socket name on windows

--- autorun-ipc/src/lib.rs
+++ autorun-ipc/src/lib.rs
@@ -8,4 +8,8 @@ pub use message::Message;
 pub use messenger::Messenger;
 pub use server::Server;
 
+#[cfg(target_os = "linux")]
 pub const SOCKET_NAME: &str = r"/tmp/autorun_ipc";
+
+#[cfg(target_os = "windows")]
+pub const SOCKET_NAME: &str = r"\\.\pipe\autorun_ipc";
