85a7240f615fb00d4afe559bfffcb871c81d14b5
Vurv <vurvdevelops@gmail.com>
2025-10-25 17:55:33 -0700

    Fix github pages base url

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://thevurv.github.io",
+	base: "/Autorun-ng/",
 	integrations: [
 		starlight({
 			title: "Autorun",
