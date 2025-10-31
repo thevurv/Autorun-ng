0c68a3f9aeba0fda21fe7a2578eb528e1f4a2eed
yogwoggf <100450992+yogwoggf@users.noreply.github.com>
2025-10-30 20:26:42 -0700

    Improve variable name

--- packages/autorun-env/src/env.rs
+++ packages/autorun-env/src/env.rs
@@ -58,10 +58,10 @@ impl EnvHandle {
 
 		lua.get_fenv(state, func_index);
 		let function_env_tvalue = index2adr(lj_state, -1).context("Failed to get TValue for function environment")?;
-		let function_env_gcobj = unsafe { (*function_env_tvalue).gcr };
+		let function_env_gcr = unsafe { (*function_env_tvalue).gcr };
 
 		lua.pop(state, 1);
-		Ok(function_env_gcobj == self.env_gcr)
+		Ok(function_env_gcr == self.env_gcr)
 	}
 
 	pub fn is_active(&self, lua: &LuaApi, state: *mut LuaState) -> bool {
