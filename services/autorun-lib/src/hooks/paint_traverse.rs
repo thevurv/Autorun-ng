c73f4b87e47c9a64ebd1c50cd5c6c39c495b1fb2
Vurv <vurvdevelops@gmail.com>
2025-10-31 21:48:15 -0700

    Refactor lua queue
    This should stop the crashes on startup, at least mostly
    
    Also, the executor now runs inside of the Autorun environment due to
    this.

--- services/autorun-lib/src/hooks/paint_traverse.rs
+++ services/autorun-lib/src/hooks/paint_traverse.rs
@@ -1,5 +1,3 @@
-use crate::lua_queue::LUA_QUEUE;
-
 type PaintTraverseFn = extern "C-unwind" fn(this: *mut std::ffi::c_void, panel: usize, force_repaint: bool, allow_force: bool);
 
 static PAINT_TRAVERSE_H: std::sync::OnceLock<retour::GenericDetour<PaintTraverseFn>> = std::sync::OnceLock::new();
@@ -10,54 +8,18 @@ extern "C-unwind" fn paint_traverse_h(this: *mut std::ffi::c_void, panel_id: usi
 		.unwrap()
 		.call(this, panel_id, force_repaint, force_allow);
 
-	let mut queue = LUA_QUEUE.lock().unwrap();
-	if queue.is_empty() {
+	let Some(callback) = crate::lua_queue::pop() else {
 		return;
-	}
-
-	let lua = autorun_lua::get_api().expect("it's over");
-
-	let (realm, source) = queue.remove(0);
-	let state = match autorun_interfaces::lua::get_state(realm).expect("it's over") {
-		Some(s) => s,
-		None => {
-			autorun_log::error!("Lua state for realm {realm:?} is not ready");
-			return;
-		}
 	};
 
 	crate::hooks::load_buffer::disable();
-	if let Err(why) = lua.load_buffer_x(state, source.as_bytes(), c"RunString", c"t") {
-		crate::hooks::load_buffer::enable();
-		autorun_log::error!("Failed to load Lua string: {why}");
-		return;
+
+	let lua = autorun_lua::get_api().unwrap();
+	if let Err(why) = callback(lua) {
+		autorun_log::error!("Error in PaintTraverse Lua callback: {}", why);
 	}
+
 	crate::hooks::load_buffer::enable();
-
-	let existing_hook = lua.get_hook(state);
-	let existing_hook_info = if existing_hook.is_null() {
-		None
-	} else {
-		Some((existing_hook, lua.get_hook_mask(state), lua.get_hook_count(state)))
-	};
-
-	if existing_hook_info.is_some() {
-		lua.set_hook(state, std::ptr::null(), 0, 0);
-	}
-
-	if let Err(why) = lua.pcall(state, 0, 0, 0) {
-		autorun_log::error!("Failed to execute Lua code: {why}");
-		return;
-	}
-
-	let did_user_set_hook = !lua.get_hook(state).is_null();
-	if did_user_set_hook {
-		autorun_log::warn!("User set a hook in executed code. This is not recommended.");
-	}
-
-	if !did_user_set_hook && let Some((hook, mask, count)) = existing_hook_info {
-		lua.set_hook(state, hook, mask, count);
-	}
 }
 
 #[allow(unused)]
--- services/autorun-lib/src/lua_queue.rs
+++ services/autorun-lib/src/lua_queue.rs
@@ -1,7 +1,19 @@
+///! The purpose of the Lua Queue is to allow other threads to schedule code to run on the main lua thread.
+///! Otherwise, if you try to run lua outside the main thread, you'll most definitely crash the game.
 use std::sync::{Arc, LazyLock, Mutex};
 
-use autorun_types::Realm;
+use autorun_lua::LuaApi;
 
-type LuaExecutionBundle = (Realm, String);
+type LuaCallback = Box<dyn FnOnce(&LuaApi) -> anyhow::Result<()> + Send + 'static>;
 
-pub static LUA_QUEUE: LazyLock<Arc<Mutex<Vec<LuaExecutionBundle>>>> = LazyLock::new(|| Arc::new(Mutex::new(vec![])));
+static LUA_QUEUE: LazyLock<Arc<Mutex<Vec<LuaCallback>>>> = LazyLock::new(|| Arc::new(Mutex::new(vec![])));
+
+pub fn push(f: impl FnOnce(&LuaApi) -> anyhow::Result<()> + Send + 'static) {
+	let mut queue = LUA_QUEUE.lock().unwrap();
+	queue.push(Box::new(f));
+}
+
+pub fn pop() -> Option<LuaCallback> {
+	let mut queue = LUA_QUEUE.lock().unwrap();
+	if queue.is_empty() { None } else { Some(queue.remove(0)) }
+}
